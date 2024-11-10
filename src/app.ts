ceb80d107e5342ee17c52f869422e291fddf4076
Patryk Popiela <patryk.popiela01@gmail.com>
2024-11-10 17:26:38 +0100

    Create connection with mongo server

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,22 @@
 import express from 'express';
+import mongoose from 'mongoose';
 import morgan from 'morgan';
 
 const PORT = process.env.PORT || 8080;
+const MONGO_CONNECTION_URI = process.env.MONGO_CONNECTION_URI!;
 
 const app = express();
-app.use(morgan('dev'))
+app.use(morgan('dev'));
 
+mongoose.connect(MONGO_CONNECTION_URI).then(() => {
+	console.log('The connection to the mongo server has been established.');
+	app.listen(PORT, () => {
+		console.log(`HTTP server listening on port: ${PORT}.`);
+	});
+});
 
-app.listen(PORT, () => {
-    console.log('HTTP server listening on port: ' + PORT);
-})
+process.on('uncaughtException', (err) => {
+	console.error('Error occured! 💥');
+	console.error(err);
+	process.exit(1);
+});
